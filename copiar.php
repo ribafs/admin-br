4cf7bb9e81b1c50b244bfd0e43401d1f698ea877
Ribamar FS <ribafs@gmail.com>
2021-05-06 11:53:59 -0300

    Update copiar.php

--- copiar.php
+++ copiar.php
@@ -1,4 +1,6 @@
 <?php
   copy ( 'copiar/AppController.php' , '../../../src/Controller/AppController.php');
   copy ( 'copiar/bootstrap_cli.php' , '../../../config/bootstrap_cli.php');
+  copy ( 'copiar/bootstrap_cli.php' , '../../../config/index.php');
+  copy ( 'copiar/bootstrap_cli.php' , '../../../config/translation.json');
 ?>
